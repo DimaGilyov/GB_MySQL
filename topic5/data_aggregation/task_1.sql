84a5c016897f32d8b1751fbe1a2d168bc6e9a905
Brotherteam <dimagilev91@gmail.com>
2021-07-17 02:51:04 +0700

    topic5

--- topic5/data_aggregation/task_1.sql
+++ topic5/data_aggregation/task_1.sql
@@ -35,6 +35,6 @@ DELIMITER ;
 CALL prepare_data;
 
 -- Запрос по задаче
-SELECT AVG(TIMESTAMPDIFF(YEAR, NOW(), birthday)) AS  average_age
+SELECT AVG(TIMESTAMPDIFF(YEAR, birthday, NOW())) AS  average_age
 FROM users;
     
